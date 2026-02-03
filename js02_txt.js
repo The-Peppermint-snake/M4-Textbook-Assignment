27a8c404c1c791d1a6ca86d4718eae481f9b4221
Beatrice Kovalik <brkovalik@gmail.com>
2026-02-03 18:41:00 -0500

    idk

--- js02_txt.js
+++ js02_txt.js
@@ -1,21 +1,29 @@
-/*    JavaScript 7th Edition
-      Chapter 2
-      Chapter case
+const EMP_COST = 100;
+const BOOK_COST = 350;
+const REPRO_COST = 1250;
+const TRAVEL_COST = 2;
 
-      Fan Trick Fine Art Photography
-      Variables and functions
-      Author: 
-      Date:   
+window.addEventListener("load", setupform);
 
-      Filename: js02.js
- */
-window.addEventListener("load", setupform)
 function setupform() {
       document.getElementById("photoNum").value = 1;
       document.getElementById("photoHrs").value = 2;
       document.getElementById("makebook").checked = false;
       document.getElementById("photoRights").checked = false;
       document.getElementById("photoDist").value = 0;
+      getEstimate();
 }
 
-<script scr="js02.js" defer></script>
+function getEstimate() {
+      let totalCost = 0;
+      let photographers = document.getElementById("photoNum").value;
+      let hours = document.getElementById("phtoHrs").value;
+      let distance = document.getElementById("photoDist").value;
+      let buyBook = document.getElementById("makeBook").checked;
+      let buyRights = document.getElementById("photoRights").checked;
+      totalCost += photographers * hours * EMP_COST;
+      totalCost += photographers * distance * TRAVEL_COST;
+      totalCost += buyBook ? BOOK_COST : 0;
+      totalCost += buyRights ? REPRO_COST : 0;
+      document.getElementById("estimate").innerHTML = "$" + totalCost;
+}
